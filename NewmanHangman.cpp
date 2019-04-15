8d4a1ad2fbc21b0ccb5647769fe65f259464c2a4
Urban158 <46946488+Urban158@users.noreply.github.com>
2019-04-14 22:26:26 -0600

    Create NewmanHangman.cpp

---
+++ NewmanHangman.cpp
@@ -0,0 +1,34 @@
+/*Program Name: Hangman
+Purpose: I am hoping to write a c++ program to imitate the classic game called Hangman.
+I have been advised that the user will have several different words which may be used for
+the game.  I assumme I will eventually learn to draw using c++ and develop a funtion that will
+draw a stick figure gallow with a stick figure person attached to. The user is expected to
+spell the word before the number of guess attempts draw the full stick figure gallow and person.
+Author: Derris Newman
+Date Last Modified: April 14, 2019 */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+/*
+Given the user is expected to spell a word before enough wrong guesses draws a stick figure and gallows,
+I am not sure how the word to be spelled will be presented to the user.  Should the user open another file
+containing a list of words of which one will be randomly chosen.
+
+Next thing I must learn to do is learn to draw to the stick figure and gallow to be used in the program.
+Once I learn how to draw the stick figure, I will have to convert to a loop file where each wrong guess will
+draw a portion of the "hangman".
+
+Should I interact with the user after each guess either with a "congratulation, you guessed a correct alphabet" or
+"sorry, guess again" and drraw a portion of the "hangman".
+
+I am finally reading and going over the Prof Wolfe and Scott McDermontt's videos for chapter 7 Arrays and Vectors.
+Hopefully this chapter will answer some of my questions on how to develop the program.
+
+
+*/
+
+  return 0;
+}
