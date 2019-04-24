9dbdf1cec10ca5124cb15f12cfe145589e64b1b2
Urban158 <46946488+Urban158@users.noreply.github.com>
2019-04-24 02:45:32 -0600

    NewmanHangman
    Hangman Pseudocode

--- NewmanHangman.cpp
+++ NewmanHangman.cpp
@@ -12,7 +12,7 @@ using namespace std;
 
 int main()
 {
-/*
+/* Initial Description
 Given the user is expected to spell a word before enough wrong guesses draws a stick figure and gallows,
 I am not sure how the word to be spelled will be presented to the user.  Should the user open another file
 containing a list of words of which one will be randomly chosen.
@@ -22,13 +22,39 @@ Once I learn how to draw the stick figure, I will have to convert to a loop file
 draw a portion of the "hangman".
 
 Should I interact with the user after each guess either with a "congratulation, you guessed a correct alphabet" or
-"sorry, guess again" and drraw a portion of the "hangman".
+"sorry, guess again" and draw a portion of the "hangman".
 
 I am finally reading and going over the Prof Wolfe and Scott McDermontt's videos for chapter 7 Arrays and Vectors.
 Hopefully this chapter will answer some of my questions on how to develop the program.
-
-
 */
+
+// Hangman Pseudocode
+// Newman Hangman main program
+// Get the player's namespace
+// Greet player and introduce game and rules
+// Ask player to hit enter to begin the game
+/* The compter will then read a file with five words to be picked for use in the game. A random selector program
+   will be used to choose the word*/
+// Potential Words include: Program, Argument, Function, and Computer.
+// The words will be entered into an array with each aplhabet/letter a subscript to the word array.
+/* Once a word is randomly selected by the computer, the screen will ilustrate an empty _ for each
+   letter of the word to be spelled with one hint below it. for example"
+
+   _ _ _ _ _ _ _ _
+   hint: An electronic device for storing and processing data, typically in binary form,
+   according to instructions given to it in a variable program
+   I have not figured out how to keep each alphapbet visual on screen while the user atempts additional guesses.
+   I am not sure if it mandatory to also show incorrectly guessed alphabets in a area away from the gallows as well*/
+
+
+// An illustration of where the gallows will be drawn will also be presented
+/* The rules will indicate that no alphabets will be repeated to spell each
+    mystery word (I can keep on screen as a reminder to user)*/
+/*  A function is under development which will print "Good Guess" for each alhpabet guess correctly and will also "else"
+   draw a portion of the gallows if guessed incorrectly with "Sorry wrong alphabet" printed with each wrong guess*/
+// The user will have up to 13 incorrect guesses before the hangman and gallow are completely drawn to end the game
+// The user will either recieve a "Congratulations you won" or "Sorry, you lose" to end the game.
+// The user will as if he/she wants to exit the game, if not, the game will repeat.
 
   return 0;
 }
