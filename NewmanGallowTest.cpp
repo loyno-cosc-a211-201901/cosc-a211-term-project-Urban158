59ca182a1a8e0a8f76c7fad98cb367b14e87918d
Urban158 <46946488+Urban158@users.noreply.github.com>
2019-04-28 10:56:03 -0600

    Gallow test

---
+++ NewmanGallowTest.cpp
@@ -0,0 +1,284 @@
+
+
+
+#include <iostream>
+using namespace std;
+
+void displayStep1 (char symbol);
+
+int main ()
+
+{
+
+char symbol;
+symbol = '*';
+
+cout << "Hello and welcome to the game Hangman!" << endl;
+cout << "Please guess each letter of the hidden word." << endl;
+cout << "No letter is repeated and one hint will be provided with each word" << end;
+
+void gallowDisplayStep1 (char symbol)
+
+
+return 0;
+}
+
+//Functions
+//********************************************************************************************************************
+// Definition of function Gallow Pedastal                                                                            *
+// First incorrect answer displays Gallow Pedastal                                                                   *
+// *******************************************************************************************************************
+
+ void gallowDisplayStep1 (char symbol)
+{
+symbol = "*";
+
+cout <<  "*" << "*" << "*" << "*" << "*" <<"*" << "*" << "*" <<"*" << "*" << "*" << "*" << "*" << "*" << "*" << endl;
+cout << "Sorry, wrong guess, please try again" << endl;
+}
+
+//********************************************************************************************************************
+// Definition of function Gallow Pedastal and Stud                                                                            *
+// Second incorrect answer displays Gallow Pedastal and Stud                                                                   *
+// *******************************************************************************************************************
+
+// void gallowDisplayStep2 (char symbol)
+// {
+// symbol = "*";
+
+//cout << "       " << "*" << endl;
+//cout << "       " << "*" << endl;
+//cout << "       " << "*" << endl;
+//cout << "       " << "*" << endl;
+//cout << "       " << "*" << endl;
+//cout << "       " << "*" << endl;
+//cout << "       " << "*" << endl;
+//cout << "       " << "*" << endl;
+//cout << "       " << "*" << endl;
+//cout << "       " << "*" << endl;
+//cout << "       " << "*" << endl;
+//cout << "       " << "*" << endl;
+//cout << "       " << "*" << endl;
+//cout <<  "*" << "*" << "*" << "*" << "*" <<"*" << "*" << "*" <<"*" << "*" << "*" << "*" << "*" << "*" << "*" << endl;
+//cout <<"Sorry, wrong guess #2, please try again" << endl;
+//}
+
+//********************************************************************************************************************
+// Definition of function Gallow Pedastal, Stud, and Beam                                                                            *
+// Third incorrect answer displays Gallow Pedastal,Stud, and Beam                                                                   *
+// *******************************************************************************************************************
+
+// void gallowDisplayStep3 (char symbol)
+// {
+// symbol = "*";
+
+//cout << "     ***********" << endl;
+//cout << "               " << "*" << endl;
+//cout << "               " << "*" << endl;
+//cout << "               " << "*" << endl;
+//cout << "               " << "*" << endl;
+//cout << "               " << "*" << endl;
+//cout << "               " << "*" << endl;
+//cout << "               " << "*" << endl;
+//cout << "               " << "*" << endl;
+//cout << "               " << "*" << endl;
+//cout << "               " << "*" << endl;
+//cout << "               " << "*" << endl;
+//cout << "               " << "*" << endl;
+//cout << "               " << "*" << endl;
+//cout <<  "*" << "*" << "*" << "*" << "*" <<"*" << "*" << "*" <<"*" << "*" << "*" << "*" << "*" << "*" << "*" << "*" << endl;
+//cout <<"Sorry, wrong guess #3, please try again" << endl;
+//}
+
+//********************************************************************************************************************
+// Definition of function Gallow Pedastal,Stud,Beam,and Rope                                                                            *
+// Fourth incorrect answer displays Gallow Pedastal,Stud,Beam,and Rope                                                                   *
+// *******************************************************************************************************************
+
+// void gallowDisplayStep4 (char symbol)
+// {
+// symbol = "*";
+
+//cout << "     ***********" << endl;
+//cout << "     *         " << "*" << endl;
+//cout << "     *         " << "*" << endl;
+//cout << "     *         " << "*" << endl;
+//cout << "               " << "*" << endl;
+//cout << "               " << "*" << endl;
+//cout << "               " << "*" << endl;
+//cout << "               " << "*" << endl;
+//cout << "               " << "*" << endl;
+//cout << "               " << "*" << endl;
+//cout << "               " << "*" << endl;
+///cout << "               " << "*" << endl;
+//cout << "               " << "*" << endl;
+//cout << "               " << "*" << endl;
+//cout <<  "*" << "*" << "*" << "*" << "*" <<"*" << "*" << "*" <<"*" << "*" << "*" << "*" << "*" << "*" << "*" << "*" << endl;
+//cout <<"Sorry, wrong guess #4, please try again" << endl;
+//}
+
+
+//********************************************************************************************************************
+// Definition of function Gallow Pedastal,Stud,Beam,Rope, and Head                                                                            *
+// Fifth incorrect answer displays Gallow Pedastal,Stud,Beam,Rope, and Head                                                                   *
+// *******************************************************************************************************************
+
+// void gallowDisplayStep5 (char symbol)
+// {
+// symbol = "*";
+
+//out << "     ***********" << endl;
+//cout << "     *         " << "*" << endl;
+//cout << "     *         " << "*" << endl;
+//cout << "     *         " << "*" << endl;
+//cout << "    ***        " << "*" << endl;
+//cout << "    ***        " << "*" << endl;
+//cout << "               " << "*" << endl;
+//cout << "               " << "*" << endl;
+//cout << "               " << "*" << endl;
+//cout << "               " << "*" << endl;
+//cout << "               " << "*" << endl;
+//cout << "               " << "*" << endl;
+//cout << "               " << "*" << endl;
+//cout << "               " << "*" << endl;
+//cout <<  "*" << "*" << "*" << "*" << "*" <<"*" << "*" << "*" <<"*" << "*" << "*" << "*" << "*" << "*" << "*" << "*" << endl;
+//cout <<"Sorry, wrong guess #5, please try again" << endl;
+//}
+
+//********************************************************************************************************************
+// Definition of function Gallow Pedastal,Stud,Beam,Rope,Head, and Torso                                                                            *
+// Sixth incorrect answer displays Gallow Pedastal,Stud,Beam,Rope,Head, and Torso                                                                   *
+// *******************************************************************************************************************
+
+// void gallowDisplayStep6 (char symbol)
+// {
+// symbol = "*";
+
+//out << "     ***********" << endl;
+//cout << "     *         " << "*" << endl;
+//cout << "     *         " << "*" << endl;
+//cout << "     *         " << "*" << endl;
+//cout << "    ***        " << "*" << endl;
+//cout << "    ***        " << "*" << endl;
+//cout << "     *         " << "*" << endl;
+//cout << "     *         " << "*" << endl;
+//cout << "     *         " << "*" << endl;
+//cout << "     *         " << "*" << endl;
+//cout << "     *         " << "*" << endl;
+//cout << "     *         " << "*" << endl;
+//cout << "               " << "*" << endl;
+//cout << "               " << "*" << endl;
+//cout <<  "*" << "*" << "*" << "*" << "*" <<"*" << "*" << "*" <<"*" << "*" << "*" << "*" << "*" << "*" << "*" << "*" << endl;
+//cout <<"Sorry, wrong guess #6, please try again" << endl;
+//}
+
+
+//********************************************************************************************************************
+// Definition of function Gallow Pedastal,Stud,Beam,Rope,Head,Torso, and Right Arm                                                                            *
+// Sixth incorrect answer displays Gallow Pedastal,Stud,Beam,Rope,Head,Torso, and Right Arm                                                                   *
+// *******************************************************************************************************************
+
+// void gallowDisplayStep7 (char symbol)
+// {
+// symbol = "*";
+//
+//cout << "     ***********" << endl;
+//cout << "     *         " << "*" << endl;
+//cout << "     *         " << "*" << endl;
+//cout << "     *         " << "*" << endl;
+//cout << "    ***        " << "*" << endl;
+//cout << "    ***        " << "*" << endl;
+//cout << "     *         " << "*" << endl;
+//cout << "     *  *      " << "*" << endl;
+//cout << "     * *       " << "*" << endl;
+//cout << "     *         " << "*" << endl;
+//cout << "     *         " << "*" << endl;
+//cout << "     *         " << "*" << endl;
+//cout << "               " << "*" << endl;
+//cout << "               " << "*" << endl;
+//cout <<  "*" << "*" << "*" << "*" << "*" <<"*" << "*" << "*" <<"*" << "*" << "*" << "*" << "*" << "*" << "*" << "*" << endl;
+//cout <<"Sorry, wrong guess #7, please try again" << endl;
+//}
+
+
+//********************************************************************************************************************
+// Definition of function Gallow Pedastal,Stud,Beam,Rope,Head,Torso,Right Arm, and Left Arm                                                                            *
+// Sixth incorrect answer displays Gallow Pedastal,Stud,Beam,Rope,Head,Torso,Right Arm, and Left Arm                                                                   *
+// *******************************************************************************************************************
+
+// void gallowDisplayStep8 (char symbol)
+// {
+// symbol = "*";
+//
+//cout << "     ***********" << endl;
+//cout << "     *         " << "*" << endl;
+//cout << "     *         " << "*" << endl;
+//cout << "     *         " << "*" << endl;
+//cout << "    ***        " << "*" << endl;
+//cout << "    ***        " << "*" << endl;
+//cout << "     *         " << "*" << endl;
+//cout << "  *  *  *      " << "*" << endl;
+//cout << "   * * *       " << "*" << endl;
+//cout << "     *         " << "*" << endl;
+//cout << "     *         " << "*" << endl;
+//cout << "               " << "*" << endl;
+//cout << "               " << "*" << endl;
+//cout <<  "*" << "*" << "*" << "*" << "*" <<"*" << "*" << "*" <<"*" << "*" << "*" << "*" << "*" << "*" << "*" << "*" << endl;
+//cout <<"Sorry, wrong guess #8, please try again" << endl;
+//}
+
+
+//********************************************************************************************************************
+// Definition of function Gallow Pedastal,Stud,Beam,Rope,Head,Torso,Right Arm,Left Arm, Right Leg                                                                            *
+// Sixth incorrect answer displays Gallow Pedastal,Stud,Beam,Rope,Head,Torso,Right Arm,Left Arm, Right Leg                                                                   *
+// *******************************************************************************************************************
+
+// void gallowDisplayStep9 (char symbol)
+// {
+// symbol = "*";
+//
+//
+//cout << "     ***********" << endl;
+//cout << "     *         " << "*" << endl;
+//cout << "     *         " << "*" << endl;
+//cout << "     *         " << "*" << endl;
+//cout << "    ***        " << "*" << endl;
+//cout << "    ***        " << "*" << endl;
+//cout << "     *         " << "*" << endl;
+//cout << "  *  *  *      " << "*" << endl;
+//cout << "   * * *       " << "*" << endl;
+//cout << "     *         " << "*" << endl;
+//cout << "     *         " << "*" << endl;
+//cout << "     *         " << "*" << endl;
+//cout << "      *        " << "*" << endl;
+//cout << "        *      " << "*" << endl;
+//cout <<  "*" << "*" << "*" << "*" << "*" <<"*" << "*" << "*" <<"*" << "*" << "*" << "*" << "*" << "*" << "*" << "*" << endl;
+//cout <<"Sorry, wrong guess #9, please try again" << endl;
+//}
+
+//********************************************************************************************************************
+// Definition of function Gallow and User                                                                            *
+// Tenth incorrect answer displays Gallow and User/ User loses game                                                                  *
+// *******************************************************************************************************************
+
+// void gallowDisplayStep10 (char symbol)
+// {
+// symbol = "*";
+//
+//cout << "     ***********" << endl;
+//cout << "     *         " << "*" << endl;
+//cout << "     *         " << "*" << endl;
+//cout << "     *         " << "*" << endl;
+//cout << "    ***        " << "*" << endl;
+//cout << "    ***        " << "*" << endl;
+//cout << "     *         " << "*" << endl;
+//cout << "  *  *  *      " << "*" << endl;
+//cout << "   * * *       " << "*" << endl;
+//cout << "     *         " << "*" << endl;
+//cout << "     *         " << "*" << endl;
+//cout << "     *         " << "*" << endl;
+//cout << "    * *        " << "*" << endl;
+//cout << "   *   *       " << "*" << endl;
+//cout <<  "*" << "*" << "*" << "*" << "*" <<"*" << "*" << "*" <<"*" << "*" << "*" << "*" << "*" << "*" << "*" << "*" << endl;
+//cout <<"Sorry incorrect guess, you lose!" << endl;
+//}
